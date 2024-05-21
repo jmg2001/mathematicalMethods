b42bbb8297a1238c4764c2e193eaac44c5ad704d
jmg2001 <jmg357159@gmail.com>
2024-05-21 09:14:53 -0600

    Actualizacion

--- scriptMethods.js
+++ scriptMethods.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
             allLinks[i].style.display = "block";
         }
         document.getElementById("verMasBtn").style.display = "none";
+        document.getElementById("backBtn").style.display = "none";
         document.getElementById("homeBtn").style.display = "block";
     }
 
@@ -22,7 +23,16 @@ document.addEventListener("DOMContentLoaded", function() {
         // Redirigir a la nueva URL
         window.location.href = "index.html";
     }
+
+    function back(){
+        // Redirigir a la nueva URL
+        window.location.href = "filter.html";
+    }
+
     document.getElementById("homeBtn").addEventListener("click", home);
+    document.getElementById("backBtn").addEventListener("click", back);
+
+    document.getElementById("verMasBtn").addEventListener("click", mostrarMasEnlaces);
 
     // Ocultar todos los enlaces iniciales (después del segundo)
     var allLinks = document.querySelectorAll(".method-column a");
@@ -32,23 +42,16 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(params);
 
     if(params.valorN == null){
-        // document.getElementById("homeBtn").style.display = "none";
-
 
         for (var i = 22; i < allLinks.length; i++) {
             allLinks[i].style.display = "none";
         }
-    
-        // Agregar evento clic al botón "Ver más"
-        document.getElementById("verMasBtn").addEventListener("click", mostrarMasEnlaces);
-        
+
+        document.getElementById("verMasBtn").style.display = "block";
+        document.getElementById("backBtn").style.display = "none";
+        document.getElementById("homeBtn").style.display = "none";
 
     }else{
-
-        // var columns = document.querySelectorAll(".method-column");
-        // for (var i = 0; i < columns.length; i++) {
-        //     columns[i].style.display = "flex";
-        // }
 
         for (var i = 0; i < allLinks.length; i++) {
             allLinks[i].style.display = "none";
@@ -57,14 +60,65 @@ document.addEventListener("DOMContentLoaded", function() {
         var links;
 
         document.getElementById("verMasBtn").style.display = "none";
+        document.getElementById("backBtn").style.display = "block";
+
 
         if(params.valorN == "sin_N"){
+            if(params.valorN == "uniforme"){
+                links = document.querySelectorAll(".category-7");
+                for (var i = 0; i < links.length; i++) {
+                    links[i].style.display = "block";
+                }
+            }else{
+                links = document.querySelectorAll(".category-8");
+                for (var i = 0; i < links.length; i++) {
+                    links[i].style.display = "block";
+                }
+            }
             
         }else{
-            links = document.querySelectorAll(".category-4");
-            for (var i = 0; i < links.length; i++) {
-                links[i].style.display = "block";
+            if(params.valorN == "par"){
+                if (params.integral == "si"){
+                    links = document.querySelectorAll(".category-1");
+                    for (var i = 0; i < links.length; i++) {
+                        links[i].style.display = "block";
+                    }
+                }else{
+                    links = document.querySelectorAll(".category-2");
+                    for (var i = 0; i < links.length; i++) {
+                        links[i].style.display = "block";
+                    }
+                }
             }
+
+            if(params.valorN == "impar"){
+                if (params.integral == "si"){
+                    links = document.querySelectorAll(".category-3");
+                    for (var i = 0; i < links.length; i++) {
+                        links[i].style.display = "block";
+                    }
+                }else{
+                    links = document.querySelectorAll(".category-4");
+                    for (var i = 0; i < links.length; i++) {
+                        links[i].style.display = "block";
+                    }
+                }
+            }
+
+            if(params.valorN == "parEimpar"){
+                if (params.integral == "si"){
+                    links = document.querySelectorAll(".category-5");
+                    for (var i = 0; i < links.length; i++) {
+                        links[i].style.display = "block";
+                    }
+                }else{
+                    links = document.querySelectorAll(".category-6");
+                    for (var i = 0; i < links.length; i++) {
+                        links[i].style.display = "block";
+                    }
+                }
+            }
+            
         }
     }    
 });
